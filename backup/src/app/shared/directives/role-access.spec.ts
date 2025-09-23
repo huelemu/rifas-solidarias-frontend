059578f8cb5b92e3cbb6880cbd3aefc89b5c6e82
juanlacy <juan.lacy@huelemu.com.ar>
2025-09-22 23:02:10 -0300

    borrar todo

--- backup/src/app/shared/directives/role-access.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RoleAccess } from './role-access';
-
-describe('RoleAccess', () => {
-  it('should create an instance', () => {
-    const directive = new RoleAccess();
-    expect(directive).toBeTruthy();
-  });
-});
